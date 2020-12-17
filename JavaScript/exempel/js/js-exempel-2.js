319d5c7b6163677a6ecddb2b847858ff40d101d9
paulwallin <steely_paul@hotmail.com>
2020-12-17 08:28:00 +0200

    javascript exempel 1 & 2

---
+++ JavaScript/exempel/js/js-exempel-2.js
@@ -0,0 +1,14 @@
+/**
+ * Javascript exempel
+ * nr. 2
+ * 
+ */
+let name = 'Paul';
+let age = 43;
+let futureAge = age + 10;
+let h1 = document.querySelector('h1.header');
+
+ console.log('Om tio år är du: ' + futureAge);
+
+ h1.innerHTML = 'Hej ' + name + ', du är ' + age + ' år gammal';
+
