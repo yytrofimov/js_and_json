59400d041c8aa176c4ac94576fab6265bac1ae02
paulwallin <steely_paul@hotmail.com>
2021-02-12 13:55:28 +0200

    funktions exempel

---
+++ JavaScript/exempel/js/js-func.js
@@ -0,0 +1,31 @@
+//exempel 1: skriv ut text
+function writeName() {
+    return 'Paul Wallin'
+}
+console.log(writeName());
+
+//exempel med parameter
+function writeNamePara(firstname, lastname) {
+    return firstname + ' ' + lastname
+}
+//vi måste ge parameter då vi kör funktionen
+console.log(writeNamePara('Paul', 'Wallin'))
+
+//Räkna area
+function countArea(num) {
+    return num*num
+}
+console.log(countArea(4))
+
+//skriver i DOM
+function writeToDom() {
+    const output = document.querySelector('#output');
+    output.innerHTML = 'testar onclick!'
+}
+
+const btn2 = document.querySelector('.btn.b');
+
+btn2.addEventListener('click', event => {
+    const output = document.querySelector('#output');
+    output.innerHTML = 'text i DOM utan onclick attributet'
+})
